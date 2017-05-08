43655d043e1a8091ad43f998caa747ec2662a88d
501986411@qq.com <501986411@qq.com>
2017-05-08 18:00:30 +0800

    SQL

--- blog.sql
+++ blog.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50617
 File Encoding         : 65001
 
-Date: 2017-05-04 18:17:37
+Date: 2017-05-08 17:59:57
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -26,13 +26,14 @@ CREATE TABLE `admin_menu` (
   `url` varchar(50) CHARACTER SET utf8 NOT NULL DEFAULT '' COMMENT '菜单连接地址',
   `sort` int(11) NOT NULL DEFAULT '0' COMMENT '排序',
   `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '状态：1:启用,2:停用',
+  `path` varchar(20) NOT NULL DEFAULT '' COMMENT '上级所有节点路径如（1-3-4）',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COMMENT='菜单数据表';
 
 -- ----------------------------
 -- Records of admin_menu
 -- ----------------------------
-INSERT INTO `admin_menu` VALUES ('1', '0', '系统设置', '', '0', '1');
-INSERT INTO `admin_menu` VALUES ('2', '1', '菜单管理', '/MenuManage/index', '1', '1');
-INSERT INTO `admin_menu` VALUES ('3', '0', '文章管理', '', '0', '1');
-INSERT INTO `admin_menu` VALUES ('4', '3', '类型管理', '', '0', '1');
+INSERT INTO `admin_menu` VALUES ('1', '0', '系统设置', '', '0', '1', '');
+INSERT INTO `admin_menu` VALUES ('2', '1', '菜单管理', '/MenuManage/index', '1', '1', '1');
+INSERT INTO `admin_menu` VALUES ('3', '0', '文章管理', '', '0', '1', '');
+INSERT INTO `admin_menu` VALUES ('4', '3', '类型管理', '', '0', '1', '3');
